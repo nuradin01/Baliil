d6cc32c7b2e8677af0825704b4cd6e529c386d59
nuradin <nuradin170@gmail.com>
2022-12-11 00:18:34 +0300

    build the frontend

---
+++ frontend/build/precache-manifest.d804f63ab575a685fcf8af9eb6b2558b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e701793053b898c27edc43720e7fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "258b04c5807700fdc035",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258b04c5807700fdc035",
+    "url": "/static/js/main.f92ba82e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
